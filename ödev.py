3a0791627c8d10424166f7582d25bee2ebecf510
melisa <78098412+melisaadurmus@users.noreply.github.com>
2022-04-14 22:49:36 +0300

    Update ödev.py

--- ödev.py
+++ ödev.py
@@ -1,28 +1,33 @@
-def compareTo(arr1, arr2) :
-    alice=[];
-    bob=[];
-    result=[];
-    sum1=0;
-    sum2=0;
-    for i in range(len(arr1)):
-        if arr1[i]<arr2[i]:
-           
-               bob.append(1)
-           
-        if arr1[i]>arr2[i]:
-            
+def compareTo(arr1, arr2):
+    alice = []
+    bob = []
+    result = []
+    sum1 = 0
+    sum2 = 0
+    if (len(arr1) != len(arr2)):
+        print("Please enter a number of the same length!")
+    else:
+        for i in range(len(arr1)):
+            if arr1[i] < arr2[i]:
+                bob.append(1)
+
+            if arr1[i] > arr2[i]:
                 alice.append(1)
-            
-        
-        
-    for i in alice:
-        sum1 = sum1 + i
-    result.append(sum1)
-    for i in bob:
-        sum2 = sum2 + i
-    result.append(sum2)
-          
 
-    return result;
+        for i in alice:
+            sum1 = sum1 + i
+        result.append(sum1)
+        for i in bob:
+            sum2 = sum2 + i
+        result.append(sum2)
 
-print(compareTo([1,4],[1,3]))
+        return result
+
+
+p1 = input("Alice Please enter numbers with spaces between them:")
+p3 = p1.split()
+p2 = input("Bob Please enter numbers with spaces between them:")
+p4 = p2.split()
+result = compareTo(p3, p4)
+if result != None:
+    print(' '.join([str(elem) for elem in result]))
